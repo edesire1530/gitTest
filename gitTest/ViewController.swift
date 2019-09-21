c45fbd8f612066f58794fc086515ae3572e0c4ed
edesire1530 <edesire1530@gmail.com>
2019-09-22 04:27:56 +0900

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by gree_mini on 2019/09/22.
+//  Copyright © 2019 gree_mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
